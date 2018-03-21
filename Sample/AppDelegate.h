664d547190feeff41c854231a6e28df0f91dd01c
wangjiuyin <1528123808@qq.com>
2018-03-21 18:59:14 +0800

    Initial Commit

---
+++ Sample/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  Sample
+//
+//  Created by wjy on 2018/3/21.
+//  Copyright © 2018年 wjy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong) NSPersistentContainer *persistentContainer;
+
+- (void)saveContext;
+
+
+@end
+
---
+++ Sample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Sample
+//
+//  Created by wjy on 2018/3/21.
+//  Copyright © 2018年 wjy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
